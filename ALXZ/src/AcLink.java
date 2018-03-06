76a90f5c4a0c0688d4fadf9feee59777a0f9b79c
Tujiao Dong <xiongzhq1997@gmail.com>
2018-03-06 21:56:07 +0800

    1.1.1

--- ALXZ/src/AcLink.java
+++ ALXZ/src/AcLink.java
@@ -4,7 +4,7 @@ public class AcLink {
 	public static void main(String[] args) throws InterruptedException {
 		Node1 node = new Node1();
 		node.record(10);
-		System.out.println("Done");
+		node.play();
 	}
 
 }
--- ALXZ/src/Node1.java
+++ ALXZ/src/Node1.java
@@ -5,9 +5,10 @@ import java.io.*;
 public class Node1 {
 	private boolean recordTrigger;
 	protected ByteArrayOutputStream bOut;
+	protected ByteArrayInputStream bIn;
 	
 	private AudioFormat getAudioFormat() {
-		float fs = 8000; // Sample Rate
+		float fs = 44100; // Sample Rate
 	    int sampleSize = 8; // Sample size in bits
 	    int channels = 1;
 	    boolean signed = true;
@@ -41,7 +42,8 @@ public class Node1 {
 				            }
 						}
 						bOut.close();
-					} catch (IOException e) {
+					}
+					catch (IOException e) {
 						System.err.println("IO exception when recording");
 						System.exit(-1);
 					}
@@ -53,14 +55,70 @@ public class Node1 {
 			recordThread.start();
 		}
 		catch (LineUnavailableException e) {
-			System.err.println("Line not available");
+			System.err.println("Line not available when recording");
 			System.exit(-2);
 		}
 	}
 	
+	private void playAudio() {
+		try {
+			final AudioFormat format = getAudioFormat();
+			byte audioArray[] = bOut.toByteArray();
+			bIn = new ByteArrayInputStream(audioArray);
+			final AudioInputStream aIn = new AudioInputStream(bIn, format, audioArray.length / format.getFrameSize());
+			DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+			final SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+			line.open(format);
+			line.start(); // Output audio to sounder
+			
+			/* Establish a thread method for playing audio */
+			Runnable player = new Runnable() {
+				int size = (int)format.getSampleRate() * format.getFrameSize();
+				byte buffer[] = new byte[size];
+				
+				public void run() {
+					try {
+						// Read bytes from input stream to buffer
+						int nBytes = aIn.read(buffer, 0, buffer.length);
+						while (nBytes != -1) {
+							if (nBytes > 0) {
+								// Write bytes from buffer to line
+								line.write(buffer, 0, nBytes);
+							}
+							// Read bytes from input stream to buffer
+							nBytes = aIn.read(buffer, 0, buffer.length);
+						}
+						// Close the line after the playing is end
+						line.drain();
+						line.close();
+					}
+					catch (IOException e) {
+						System.err.println("IO exception when playing");
+						System.exit(-3);
+					}
+				}
+			};
+			
+			/* Construct a thread for playing audio */
+			Thread playThread = new Thread(player);
+		    playThread.start();
+		}
+		catch (LineUnavailableException e) {
+			System.err.println("Line not available when playing");
+			System.exit(-4);
+		}
+	}
+	
 	public void record(int time) throws InterruptedException {
+		System.out.println("Now recording");
 		recordAudio();
 		TimeUnit.SECONDS.sleep(time);
 		recordTrigger = false;
-	}	
+		System.out.println("End recording");
+	}
+	
+	public void play() {
+		System.out.println("Now playing");
+		playAudio();
+	}
 }
